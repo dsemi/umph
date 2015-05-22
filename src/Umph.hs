06cf8403b771d42bbcb16c4c2000889598dd5830
Dan Seminara <semi225599@gmail.com>
2015-05-21 22:48:13 -0400

    change to v3 youtube data api

--- src/Umph.hs
+++ src/Umph.hs
@@ -1,39 +1,75 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 module Main where
 
+import API
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Maybe
 import Data.Aeson
+import Data.List (unfoldr)
+import Data.Maybe
+import Debug.Trace
+import Formatting
 import Network.HTTP.Conduit
-
-testUrl = "https://gdata.youtube.com/feeds/api/playlists/PL_XqGBfpM20gTwBHV-yCoKwJpayIy-jzL?alt=json&start-index=1&max-results=50&v=2"
-
-data Page = Page { totalEntries :: Integer
-                 , entries :: [Link]
-                 } deriving (Show)
+import System.Environment
 
 newtype Link = Link String
 
-instance FromJSON Page where
-    parseJSON (Object v) = v .: "feed" >>= 
-                           \x -> Page <$> (x .: "openSearch$totalResults" >>= (.: "$t"))
-                                 <*> (x .: "entry")
+data Page = Page { nextPageToken :: Maybe String
+                 , entries :: [Link]
+                 } deriving (Show)
+
+data UserPage = UserPage { playlistId :: String
+                         } deriving (Show)
 
 instance FromJSON Link where
-    parseJSON (Object v) = Link <$> ((head <$> v .: "link") >>= (.: "href"))
+    parseJSON (Object v) = Link . formatToString videoUrl
+                           <$> ((v .: "contentDetails") >>= (.: "videoId"))
 
 instance Show Link where
     show (Link s) = s
 
-downloadPage :: String -> MaybeT IO Page
+instance FromJSON Page where
+    parseJSON (Object v) = Page <$> (v .:? "nextPageToken") <*> (v .: "items")
+
+instance Monoid Page where
+    mempty = Page Nothing []
+    mappend (Page _ b) (Page c d) = Page c $ b ++ d
+
+instance FromJSON UserPage where
+    parseJSON (Object v) = UserPage <$> (head <$> (v .: "items") >>= (.: "contentDetails") >>= (.: "relatedPlaylists") >>= (.: "uploads"))
+
+playlistUrl   = "https://www.googleapis.com/youtube/v3/playlistItems?key=" % string % "&part=contentDetails&playlistId=" % string % "&maxResults=" % int
+uploadUrl     = "https://www.googleapis.com/youtube/v3/channels?key=" % string % "&part=contentDetails&forUsername=" % string
+videoUrl      = "https://www.youtube.com/watch?v=" % string
+testPlaylist = "PL_XqGBfpM20gxIT0C3gA68eVe-LS7iFrC"
+
 downloadPage url = lift (decode <$> simpleHttp url) >>= MaybeT . return
 
-printLinks :: Page -> IO ()
-printLinks = mapM_ print . entries
+downloadPlaylist :: String -> Int -> MaybeT IO Page
+downloadPlaylist id' maxResults = downloadAllPages $ Just ""
+    where url = formatToString playlistUrl youtubeApiKey id' maxResults
+          download = downloadPage . (url ++) . maybe "" ("&pageToken=" ++)
+          downloadAllPages Nothing = return mempty
+          downloadAllPages token   = do
+            page <- download token
+            (page `mappend`) <$> downloadAllPages (nextPageToken page)
+
+downloadUserUploads :: String -> MaybeT IO Page
+downloadUserUploads username = do
+  id' <- playlistId <$> downloadPage url
+  downloadPlaylist id' 50
+    where url = formatToString uploadUrl youtubeApiKey username
+
+printLinks :: Maybe Page -> IO ()
+printLinks = maybe (return ()) (mapM_ print . entries)
+
+-- Just need to argparse
 
 main = do
-  runMaybeT $ downloadPage testUrl >>= lift . printLinks
-  return ()
+  -- mPage <- runMaybeT $ downloadPlaylist testPlaylist 50
+  -- printLinks mPage
+  uPage <- runMaybeT $ downloadUserUploads "birgirpall"
+  printLinks uPage
